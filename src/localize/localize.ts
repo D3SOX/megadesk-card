d3082f507757ddf08bc91c3e073dc3553fc577ac
D3SOX <nico@d3sox.me>
2025-05-25 02:45:57 +0200

    fix

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,16 +1,8 @@
 import * as en from './languages/en.json';
-import * as uk from './languages/uk.json';
-import * as de from './languages/de.json';
-import * as fr from './languages/fr.json';
-import * as pl from './languages/pl.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   en,
-  uk,
-  de,
-  fr,
-  pl,
 };
 
 export function localize(string: string, search = '', replace = ''): string {
